0b741a65e59d05900c09e5d75ab97ed95c56dca4
Parikshit Joon <parikshit.joon@gmail.com>
2018-11-20 14:36:37 +0530

    Action modal header (#25)
    * Action Modal, now augmented with a header and a headerType prop.
    
    * Code cleanup

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -16,19 +16,27 @@ const colors = {
     light: "#FDF2DA"
   },
   blue: {
-    base: "#6161FF",
+    base: "#1F69FF",
     light: "#DDE8FF"
   },
   white: {
     base: "#ffffff"
   },
   emerald: {
+    darker: "#088649",
+    dark: "#0E9A56",
     base: "#10AC60",
-    light: "#DBF3E7"
+    light: "#40BD80",
+    lighter: "#9DDCBE",
+    lightest: "#DBF3E7"
   },
   red: {
+    darker: "#A91D00",
+    dark: "#BD2900",
     base: "#D32F02",
-    light: "#F9E0D9"
+    light: "#D94C26",
+    lighter: "#EFBBAD",
+    lightest: "#F9E0D9"
   },
   purple: {
     base: "#9254E3",
@@ -43,7 +51,7 @@ const colors = {
     light: "#E4F6F6"
   },
   coral: {
-    base: "#E4F6F6",
+    base: "#ED754B",
     light: "#FDEBE4"
   },
   jade: {
