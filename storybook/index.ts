c1075dc34d3f191de606f3cda01a990cf1e8a35b
Aziz Khambati <aziz.h.khambati@gmail.com>
2020-04-15 03:16:17 +0530

    Upgrade expo and React Native

--- storybook/index.ts
+++ storybook/index.ts
@@ -1,3 +1,16 @@
-import StorybookUI from "./storybook";
+import { getStorybookUI, configure } from "@storybook/react-native";
+import AsyncStorage from "@react-native-community/async-storage";
+import "./rn-addons";
 
-export default StorybookUI as any;
+// import stories
+configure(() => {
+  require("./stories");
+}, module);
+
+// Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
+// To find allowed options for getStorybookUI
+const StorybookUIRoot = getStorybookUI({
+  asyncStorage: AsyncStorage
+});
+
+export default StorybookUIRoot;
